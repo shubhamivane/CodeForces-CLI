03e84710c2134832a64637ac2fbe4c25a7b34118
Shubham Ivane <ivaneshubham@gmail.com>
2019-06-26 14:59:18 +0530

    CLI boilerplate

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'cf_cli',
+    version = '1.0',
+    py_modules = ['cf'],
+    install_requires = [
+        'Click',    
+    ],
+    entry_points = '''
+        [console_scripts]
+        cf=cf.main:cli
+    ''', 
+)

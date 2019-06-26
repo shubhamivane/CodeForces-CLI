c87cec5e7ace8a5bc9cf06ce6b1d3e3c74bfe6fc
Shubham Ivane <ivaneshubham@gmail.com>
2019-06-26 15:23:50 +0530

    login is implemented

--- cfcli/crawler/login.py
+++ cfcli/crawler/login.py
@@ -19,31 +19,45 @@ headers = {
         'User-Agent'  : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'        
 }
 
-with requests.Session() as session:
-    url = 'https://codeforces.com/enter'
-    response = session.get(url, headers = headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    user_data['csrf_token'] = soup.find('input', {'name' : 'csrf_token'})['value']
-    response = session.post(url, data = urlencode(user_data), headers = headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    response = session.get('https://codeforces.com/profile/ivane_shubham')
-    soup = BeautifulSoup(response.content, 'html.parser')
-    print(soup.prettify())    
-
-def get_CSRF_token(url, headers):
-	try:
-		response = requests.get(url, headers)
-		soup = BeautifulSoup(response.content, 'html.parser')
-		csrf_token = soup.find('input', {'name': 'csrf_token'})
-		if csrf_token == None:
-			return False, "Could not get token"
-		else:
-			return True, csrf_token['value']
-	except 
-
-def verify_user_credentials(username, password):
+def get_CSRF_token(url):
+    try:
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content)
+        csrf_token = soup.find('input', {'name': 'csrf_token'})['value']
+        return csrf_token, ''
+    except requests.exceptions.Timeout as error:
+        return None, error
+    except requests.exceptions.TooManyRedirects as error:
+        return None, error
+    except requests.exceptions.HTTPError as error:
+        return None, error
+    
+def login(username, password):
     user_data['handleOrEmail'] = username
     user_data['password'] = password
-    with requests.Session() as session:
-        url = 'https://codeforces.com/enter'
-        user_data['csrf_token'] = get_CSRF_token(url, headers)
+    try:
+        with requests.Session() as session:
+            url = 'https://codeforces.com/enter'
+            csrf_token, error = get_CSRF_token(url)
+            if not csrf_token is None:
+                user_data['csrf_token'] = csrf_token
+                response = session.post(url, data = urlencode(user_data), headers = headers)
+                if response.status_code == 302:
+                    return session, ''
+                else:
+                    return None, 'Invalid Handle/Email address or password'
+            else:
+                return False, error
+    except requests.exceptions.Timeout as error:
+        return None, error
+    except requests.exceptions.TooManyRedirects as error:
+        return None, error
+    except requests.exceptions.HTTPError as error:
+        return None, error
+
+def verify_credentials(username, password):
+    session, error = login(username, password)
+    if session is None:
+        return False, error
+    else:
+        return True, ''
